c0ed1407de4c425504f8d9bbbb1db93b8a17dc42
noset <342947998@qq.com>
2020-05-24 01:15:50 +0800

    通过创建代理支持Task返回值的远程方法

--- src/AsyncRemoting.Service/IEmployeeService.cs
+++ src/AsyncRemoting.Service/IEmployeeService.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System.Security;
+using System.Threading.Tasks;
+
+// 必须加这个特性，不然委托跟方法绑定的时候会抛出异常
+[assembly: SecurityRules(SecurityRuleSet.Level1)]
 
 namespace AsyncRemoting.Service
 {
